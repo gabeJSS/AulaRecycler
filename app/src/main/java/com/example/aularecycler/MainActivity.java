263ed18a2851faef54e3f3cd01bc978606834d38
GABRIEL JOSÉ DE SOUSA STEINBACH <gabriel_j_steinbach@estudante.sesisenai.org.br>
2023-05-10 09:16:49 -0300

    atualizando tudo

--- app/src/main/java/com/example/aularecycler/MainActivity.java
+++ app/src/main/java/com/example/aularecycler/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.aularecycler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList <Produto> listaProdutos = new ArrayList<>();
+    ArrayList<Produto> listaProdutos = new ArrayList<>();
+    RecyclerView recycler;
+    Adaptador adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        cadastroInicial();
+        recycler = findViewById(R.id.rv);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adaptador(this, listaProdutos, new Adaptador.OnItemClickListener() {
+            @Override
+            public void onItemClick(Produto p) {
+                Toast.makeText(MainActivity.this, p.getNome(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        recycler.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
     }
     public void cadastroInicial(){
         Produto p1 = new Produto("Arroz", "Alimento",(float)7.99);
@@ -29,6 +49,16 @@ public class MainActivity extends AppCompatActivity {
         listaProdutos.add(p5);
         listaProdutos.add(p6);
         listaProdutos.add(p7);
+    }
+    public void telacadastro(View v){
+        Intent i = new Intent(this, cadastro.class);
+        startActivity(i);
+        cadastro.ListaDeProdutos = listaProdutos;
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/aularecycler/Produto.java
+++ app/src/main/java/com/example/aularecycler/Produto.java
@@ -1,16 +1,36 @@
 package com.example.aularecycler;
 
 public class Produto {
-    String nome, tipo;
+    String nome, categoria;
     float preco;
 
-    public Produto(String nome, String tipo, float preco) {
+    public Produto(String nome, String categoria, float preco) {
         this.nome = nome;
-        this.tipo = tipo;
+        this.categoria = categoria;
         this.preco = preco;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
     }
 }
